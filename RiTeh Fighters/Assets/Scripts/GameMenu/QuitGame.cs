6c5a2407566e1e208bbcabce9b5488f18b631f23
Antonio Cvitkovic <ancvitko.dev@gmail.com>
2023-04-06 15:31:11 +0200

    MainMenu Change
    Added the exit button functionality.

---
+++ RiTeh Fighters/Assets/Scripts/GameMenu/QuitGame.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This code does not seem to work within the editor to exit the play state. After compiling, it works perfectly within the game.
+public class QuitGame : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
